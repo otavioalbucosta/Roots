c728e1eb69eb6860ab8dd5fac225735a7ee1e754
otavioalbucosta <otavio-albucosta@hotmail.com>
2023-02-01 10:56:36 -0300

    hitboxes started

--- Assets/SwordAttack.cs
+++ Assets/SwordAttack.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class SwordAttack : MonoBehaviour
 {
     // Start is called before the first frame update
-    Collider2D attackCollider;
+    Vector2 HorizontalOffset;
+    Vector2 HorizontalSize;
+    Vector2 VerticalBottomOffset = new Vector2(0.03f,-0.17f);
+    Vector2 VerticalTopOffset = new Vector2(0, -0.05f);
+    Vector2 VerticalSize = new Vector2(0.1973216f,0.144767f);
+
+
+    BoxCollider2D attackCollider;
     void Start()
     {
         attackCollider = GetComponent<BoxCollider2D>();
+        HorizontalOffset = attackCollider.offset;
+        HorizontalSize = attackCollider.size;
     }
 
     void attackLeft(){
         attackCollider.enabled = true;
+        attackCollider.offset = new Vector2(-HorizontalOffset.x, HorizontalOffset.y);
+        attackCollider.size = HorizontalSize;
     }
     void attackRight() {
-            attackCollider.enabled = true;
+        attackCollider.enabled = true;
+        attackCollider.offset = HorizontalOffset;
+        attackCollider.size = HorizontalSize;
     }
     void attackTop() {
-            attackCollider.enabled = true;
+        attackCollider.enabled = true;
+        attackCollider.offset = VerticalTopOffset;
+        attackCollider.size = VerticalSize;
     }
     void attackBottom() {
         attackCollider.enabled = true;
+        attackCollider.offset = VerticalBottomOffset;
     }
     void stopAttacking() {
         attackCollider.enabled = false;
